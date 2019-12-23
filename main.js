eae3c4fa624c16b834f289d638904acbed25ebb8
Azzryel <AzryelShroud@gmail.com>
2019-12-23 17:05:09 +0100

    add win draw check function

--- main.js
+++ main.js
@@ -27,20 +27,50 @@ const game = ( () => {
     let firstPlayed = false;
     let mark;
     let win = false;
+    let drawCheck = 0;
 
-   
+    const checkWin = () => {
+        drawCheck++;
+        if (( gameBoard.gameBoard[0] == "X" && gameBoard.gameBoard[1] == "X" && gameBoard.gameBoard[2] == "X"  ) 
+            || ( gameBoard.gameBoard[3] == "X" && gameBoard.gameBoard[4] == "X" && gameBoard.gameBoard[5] == "X"  ) 
+            || ( gameBoard.gameBoard[6] == "X" && gameBoard.gameBoard[7] == "X" && gameBoard.gameBoard[8] == "X"  )
+            || ( gameBoard.gameBoard[0] == "X" && gameBoard.gameBoard[4] == "X" && gameBoard.gameBoard[8] == "X"  ) 
+            || ( gameBoard.gameBoard[2] == "X" && gameBoard.gameBoard[4] == "X" && gameBoard.gameBoard[6] == "X"  )
+            || ( gameBoard.gameBoard[0] == "X" && gameBoard.gameBoard[3] == "X" && gameBoard.gameBoard[6] == "X"  )
+            || ( gameBoard.gameBoard[1] == "X" && gameBoard.gameBoard[4] == "X" && gameBoard.gameBoard[7] == "X"  ) 
+            || ( gameBoard.gameBoard[2] == "X" && gameBoard.gameBoard[5] == "X" && gameBoard.gameBoard[8] == "X"  ) ) {
+            alert("WIN")
+            win = true;
+        }
+        else if (( gameBoard.gameBoard[0] == "O" && gameBoard.gameBoard[1] == "O" && gameBoard.gameBoard[2] == "O"  ) 
+            || ( gameBoard.gameBoard[3] == "O" && gameBoard.gameBoard[4] == "O" && gameBoard.gameBoard[5] == "O"  ) 
+            || ( gameBoard.gameBoard[6] == "O" && gameBoard.gameBoard[7] == "O" && gameBoard.gameBoard[8] == "O"  )
+            || ( gameBoard.gameBoard[0] == "O" && gameBoard.gameBoard[4] == "O" && gameBoard.gameBoard[8] == "O"  ) 
+            || ( gameBoard.gameBoard[2] == "O" && gameBoard.gameBoard[4] == "O" && gameBoard.gameBoard[6] == "O"  )
+            || ( gameBoard.gameBoard[0] == "O" && gameBoard.gameBoard[3] == "O" && gameBoard.gameBoard[6] == "O"  )
+            || ( gameBoard.gameBoard[1] == "O" && gameBoard.gameBoard[4] == "O" && gameBoard.gameBoard[7] == "O"  ) 
+            || ( gameBoard.gameBoard[2] == "O" && gameBoard.gameBoard[5] == "O" && gameBoard.gameBoard[8] == "O"  ) ) {
+            alert("WIN")
+            win = true;
+        }
+        else if (drawCheck == 9) {
+            alert("Draw!")
+        }
+
+
+    }
 
     const play = () => {
         board.forEach( (place) => {
             place.addEventListener ("click", (e) => {
                 const position = e.target.getAttribute("data-place");
                 firstPlayed ? mark = player2.mark : mark = player1.mark;
-
+                if (win == true) return;
                 if (gameBoard.gameBoard[position] == "") {
                     gameBoard.gameBoard[position] = `${mark}`;
                     firstPlayed ? firstPlayed = false : firstPlayed = true;
                     gameBoard.render();
-
+                    checkWin();
                 }
                 else {
                     alert("Place taken!")
