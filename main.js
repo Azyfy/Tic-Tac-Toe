9354c40ad1a7800ce9bda11c74ad0ca6d408ca78
Azzryel <AzryelShroud@gmail.com>
2019-12-23 16:41:53 +0100

    add place X O on click. change images

--- main.js
+++ main.js
@@ -1,20 +1,56 @@
-
-
+"use strict";
 const gameBoard = ( () => { 
-    const gameBoard = ["x", "y", "X", "y", "Y", "x", "X", "y", ""];
-    return { gameBoard };
+    const gameBoard = ["", "", "", "", "", "", "", "", ""];
+
+    const render = () => {
+        for (let i=0; i<9; i++) {
+            const place = document.querySelector(`#ttt-${i}`);
+            const img = `<img src="./img/${gameBoard[i]}.png" alt="${gameBoard[i]}" class="XO">`;
+            if (gameBoard[i] != "") {
+            place.innerHTML = `${img}`;
+            } 
+        }
+    }
+
+    return { gameBoard, render };
 }) ();
 
-const Player = (name) => {
-    return name;
+const Player = (name, mark) => {
+    return { name, mark};
 };
 
-function renderGameBoard () {
-    for (let i=0; i<9; i++) {
-        const place = document.querySelector(`#ttt-${i}`);
-        place.textContent = `${gameBoard.gameBoard[i]}`; 
+const player1 = Player("MrX", "X");
+const player2 = Player("MrY", "O");
+
+const game = ( () => { 
+    const board = document.querySelectorAll(".ttt-place"); 
+    let firstPlayed = false;
+    let mark;
+    let win = false;
+
+   
+
+    const play = () => {
+        board.forEach( (place) => {
+            place.addEventListener ("click", (e) => {
+                const position = e.target.getAttribute("data-place");
+                firstPlayed ? mark = player2.mark : mark = player1.mark;
+
+                if (gameBoard.gameBoard[position] == "") {
+                    gameBoard.gameBoard[position] = `${mark}`;
+                    firstPlayed ? firstPlayed = false : firstPlayed = true;
+                    gameBoard.render();
+
+                }
+                else {
+                    alert("Place taken!")
+                }
+                
+            });
+        });
     }
-};
 
-renderGameBoard()
-renderGameBoard()
+    return { play };
+}) ();
+
+game.play()
